f55566f48355f6186bc2df5bdad108c3af184295
julio-crypt0 <hernandezjulio58@ymail.com>
2019-09-30 08:33:31 -0700

    step 4

--- src/api.js
+++ src/api.js
@@ -1,9 +1,21 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
 
-const getItems() {
-  return Promise.resolve('A successful response!');
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/julio';
+
+function getItems(){
+  return fetch(`${BASE_URL}/items`)
+ // return Promise.resolve('A successful response!');
+};
+
+function createItem(name) {
+  let newItem = JSON.stringify({name})
+  fetch(`${BASE_URL}/items`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: newItem
+  });
 }
 
 export default {
-  getItems
+  getItems,
+  createItem
 };

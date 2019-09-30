b1a1a83a88d5d4a625affd93702587307333021a
julio-crypt0 <hernandezjulio58@ymail.com>
2019-09-30 08:45:08 -0700

    step cinco

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import api from './api';
+import store from './store';
 
 import 'normalize.css';
 import './index.css';
@@ -7,8 +8,12 @@ import './index.css';
 import shoppingList from './shopping-list';
 
 const main = function () {
-  api.createItem
-
+  api.getItems()
+  .then(res => res.json())
+  .then((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
